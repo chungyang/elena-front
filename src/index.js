074bd186e11eae9f58c92dd2ecb5671473334390
chung <chung.yang@engineer.uconn.edu>
2020-03-24 13:02:21 -0400

    create react project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
+
+import React from "react";
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Polyline, Map, TileLayer, FeatureGroup} from "react-leaflet";
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class ElenaMap extends React.Component {
+  constructor() {
+    super()
+    this.mapRef = React.createRef();
+    this.groupRef = React.createRef();
+    this.state = {
+      center: [42.361145, -71.057083],
+      zoom: 20,
+      highlighted_route: [[42.704202, -71.502017],
+	       [42.7036844, -71.5020453],
+	       [42.7035846, -71.5020392]]
+    }
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+     const map = this.mapRef.current.leafletElement;  //get native Map instance
+     console.log(map)
+     // map.fitBounds(group.getBounds());
+  }
+
+  render() {
+    const position = [this.state.center[0], this.state.center[1]];
+    return (
+        <Map center={position} zoom={this.state.zoom}  ref={this.mapRef}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            boundsOptions={this.state.highlighted_route}
+          />
+          <FeatureGroup ref={this.groupRef}>
+            <Polyline color="blue" positions={this.state.highlighted_route}/>
+          </FeatureGroup>
+        </Map>
+    );
+  }
+}
+
+class SearchBar extends React.Component{
+  render(){
+    return (
+      <form action="" method="get" >
+        <div className="div_pad"></div>
+        <div className="form-group row" id="search_form">
+          <input className="form-control" type="text" name="origin" id="from" required placeholder="From"/>
+        </div>
+        <div className="form-group row" id="search_form">
+          <input className="form-control" type="text" name="destination" id="to" required placeholder="To"/>
+        </div>
+        <div className="form-example" id="search_form">
+          <input className="btn btn-primary" type="submit" value="Search"/>
+        </div>
+      </form>
+    );
+  }
+
+}
+
+
+ReactDOM.render(<ElenaMap />, document.getElementById('root'))
+ReactDOM.render(<SearchBar />, document.getElementById('form'))
