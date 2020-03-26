e8d681e0379d5517e49b22e04011afa43336cf42
chung <chung.yang@engineer.uconn.edu>
2020-03-26 18:35:42 -0400

    fit to the highlighted route when it's rendered

--- src/index.js
+++ src/index.js
@@ -5,79 +5,63 @@ import { Polyline, Map, TileLayer, FeatureGroup} from "react-leaflet";
 import 'bootstrap/dist/css/bootstrap.css';
 
 class ElenaMap extends React.Component {
+
   constructor() {
     super()
-    this.mapRef = React.createRef();
-    this.groupRef = React.createRef();
     this.state = {
       center: [42.361145, -71.057083],
-      zoom: 20
+      zoom: 20,
+      highlighted_route: []
     }
+
+    this.routeRef = React.createRef();
+    this.mapRef = React.createRef();
   }
 
-  // componentDidMount(prevProps) {
-  //    var map = this.mapRef.current;
-  //    var featureGroup = this.groupRef.current;
-  //    console.log(map);
-  //    console.log(featureGroup);
-  //    if(map !== null && featureGroup !== null){
-  //      map = map.leafletElement
-  //      featureGroup = featureGroup.leafletElement
-  //      map.fitBounds(featureGroup.getBounds());
-  //    }
-  // }
+  highlighted_route = (route) => {
+    const mapElement = this.mapRef.current.leafletElement;
+    const routeElement = this.routeRef.current.leafletElement;
+    console.log(mapElement)
+    console.log(routeElement)
+    this.setState({highlighted_route:route.values});
+    mapElement.fitBounds(routeElement.getBounds());
+  }
 
   render() {
     const position = [this.state.center[0], this.state.center[1]];
     return (
       <div>
-      <SearchBar />
+      <SearchBar onGetRoute={this.highlighted_route}/>
       <Map center={position} zoom={this.state.zoom}  ref={this.mapRef}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        <HighlightedRoute />
+        <FeatureGroup>
+          <Polyline color="blue" positions={this.state.highlighted_route} ref={this.routeRef}/>
+        </FeatureGroup>
       </Map>
       </div>
     );
   }
 }
 
-class HighlightedRoute extends React.Component{
-
-  constructor(){
-    super()
-    this.state = {
-      highlighted_route: []
-    }
-  }
-
-  render(){
-    return(
-      <FeatureGroup>
-        <Polyline color="blue" positions={this.state.highlighted_route}/>
-      </FeatureGroup>
-    );
-  }
-}
 
 class SearchBar extends React.Component{
 
-  constructor(){
-    super();
+  constructor(props){
+    super(props);
     this.state = {
       from : "",
       to : "",
     }
   }
 
-
   submitHandler = (event) =>{
     event.preventDefault();
     fetch("http://localhost:8080/" + this.state.from + "/" + this.state.to)
       .then(response => {return response.json();})
-      .then(data => console.log(data))
+      .then(data => this.props.onGetRoute(data))
   }
 
   changeHandler = (event) =>{
