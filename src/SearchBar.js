d02099c0d95b246f8bece9d75e6a3f7a992ddf28
chung <chung.yang@engineer.uconn.edu>
2020-04-08 21:17:04 -0400

    use material-ui autocomplete

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,11 +1,26 @@
 import React from "react";
 import { Button } from 'react-bootstrap'
 import AlgorithmMenu from './AlgorithmMenu'
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 class SearchBar extends React.Component{
 
   constructor(props){
     super(props);
+    this.top100Films = [];
+
+    this.useStyles = makeStyles((theme) => ({
+      root: {
+        '& .MuiTextField-root': {
+          margin: theme.spacing(1),
+          width: 200,
+        },
+      },
+    }));
+
     this.state = {
       from : "",
       to : "",
@@ -23,48 +38,75 @@ class SearchBar extends React.Component{
     uri.searchParams.append("algorithm", this.state.algorithm)
     uri.searchParams.append("elemode", this.state.elevationMode)
     uri.searchParams.append("percentage", this.state.percentage)
-
+    alert(uri.href)
     fetch(uri.href)
       .then(response =>  {return response.json();})
       .then(data => this.props.onGetRoute(data))
       .catch(error => alert("something went wrong"))
   }
 
-  changeHandler = (event) =>{
-    const name = event.target.id
-    const value = event.target.value
+  changeHandler = (event, event_value) =>{
+    const name = event.target.id.split("-")[0]
+    const value = event_value?  event_value : event.target.value
+
     this.setState({
       [name]: value,
     })
+  }
+
+  autocompleteSelectionHandler = (selection)=>{
+    console.log(selection)
   }
 
   algoMenuSelectHandler = (selection) =>{
     this.setState({[selection.key]:selection.value})
   }
 
+
   render(){
     return (
-      <div>
-        <form onSubmit={this.submitHandler} id = "search_form">
-          <div className="pad_top"></div>
-          <div className="form-group row" >
-            <input className="form-control"  type="text"
-             id="from" required placeholder="From" onChange={this.changeHandler}/>
-          </div>
-          <div className="form-group row" >
-            <input className="form-control" type="text"
-            id="to" required placeholder="To" onChange={this.changeHandler}/>
-          </div>
-          <div className="form-group row" >
-            <input className="form-control" type="text"
-            id="percentage" required placeholder="Shortest Path %" onChange={this.changeHandler}/>
-          </div>
-          <AlgorithmMenu onSelect={this.algoMenuSelectHandler}/>
-          <div className="pad_top" id="submit_button">
-            <Button variant="outline-primary" type="submit">Search</Button>
-          </div>
-        </form>
-      </div>
+
+      <form onSubmit={this.submitHandler} className={this.useStyles.root} >
+        <div id="search_form">
+          <Autocomplete
+             freeSolo
+             options={this.top100Films.map((option) => option.title)}
+             id="from"
+             onChange={this.changeHandler}
+             renderInput={(params) => (
+               <TextField
+                 {...params}
+                 label="Origin"
+                 margin="normal"
+                 variant="outlined"
+                 onChange={this.changeHandler}
+                 InputProps={{ ...params.InputProps, type: 'from_search' }}
+               />
+             )}
+           />
+            <Autocomplete
+              freeSolo
+              options={this.top100Films.map((option)=>option.title)}
+              id="to"
+              onChange={this.changeHandler}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Destination"
+                  margin="normal"
+                  variant="outlined"
+                  onChange={this.changeHandler}
+                  InputProps={{ ...params.InputProps, type: 'to_search'}}
+                />
+              )}
+            />
+            <AlgorithmMenu onSelect={this.algoMenuSelectHandler}/>
+            <div className="pad_top" id="submit_button">
+              <Button variant="outline-primary" type="submit">Search</Button>
+            </div>
+        </div>
+
+      </form>
     );
   }
 }
